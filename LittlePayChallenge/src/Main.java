3d6a56e1dbaebe77b1b32811562b9ecc8f6d5f17
skanderellouze94 <skanderellouze94@gmail.com>
2023-11-25 11:02:05 +0100

    Ãrun server on port 8888 and print clientSocket

--- LittlePayChallenge/src/Main.java
+++ LittlePayChallenge/src/Main.java
@@ -1,6 +1,24 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    final int portNumber = 8888;
+
+    try {
+      ServerSocket serverSocket = new ServerSocket(portNumber);
+      System.out.println("Server listening on port " + portNumber);
+
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connected: " + clientSocket.toString());
+
+      }
+    }catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
+
 }
