d76ef574eb56af59ebe1e240ddc8975f45288528
skanderellouze94 <skanderellouze94@gmail.com>
2023-11-25 11:54:39 +0100

    reading message from request

--- LittlePayChallenge/src/Main.java
+++ LittlePayChallenge/src/Main.java
@@ -1,24 +1,9 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import Server.Server;
 
 public class Main {
 
   public static void main(String[] args) {
-    final int portNumber = 8888;
-
-    try {
-      ServerSocket serverSocket = new ServerSocket(portNumber);
-      System.out.println("Server listening on port " + portNumber);
-
-      while (true) {
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client connected: " + clientSocket.toString());
-
-      }
-    }catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    Server server = new Server(8888);
+    server.startServer();
   }
-
 }
